fb0793d3b9aa707f2b7956815ef3e6b512266106
Howard-12 <s0989175085@gmail.com>
2024-11-29 12:52:18 +1000

    change gui framwork to iced instead. Can present window with nothing on the screen

--- src/main.rs
+++ src/main.rs
@@ -3,8 +3,7 @@ mod ui;
 use ui::gui::AppCore;
 
 
-fn main() {
-    let native_options = eframe::NativeOptions::default();
-    let _ = eframe::run_native("Limiinal", native_options, Box::new(|cc| Ok(Box::new(AppCore::new(cc)))));
+fn main() -> iced::Result {
+    iced::run("Limiinal", AppCore::update, AppCore::view)
 }
 
