016a68034f63bad7bbcfa71209e4541b772308ce
smp46 <27676057+smp46@users.noreply.github.com>
2024-12-01 11:46:54 +1000

    Intergrated ping tutorial from docs, trying to asyncrhonously call

--- src/main.rs
+++ src/main.rs
@@ -4,10 +4,55 @@ use ui::gui::AppCore;
 
 use iced::Theme;
 
-fn main() -> iced::Result {
+use futures::prelude::*;
+use libp2p::swarm::SwarmEvent;
+use libp2p::{ping, Multiaddr};
+use std::error::Error;
+use std::time::Duration;
+use tracing_subscriber::EnvFilter;
+
+#[tokio::main]
+async fn main() -> iced::Result {
+    ping().await;
+
     iced::application("Limiinal", AppCore::update, AppCore::view)
         //.subscription(AppCore::subscribtion)
         .theme(|_| { Theme::Dark })
-        .run()
+        .run();
+    Ok(())
 }
 
+async fn ping() -> Result<(), Box<dyn Error>> {
+    tracing_subscriber::fmt().with_env_filter(EnvFilter::from_default_env()).init();
+
+    let mut swarm = libp2p::SwarmBuilder::with_new_identity()
+        .with_async_std()
+        .with_tcp(
+            libp2p::tcp::Config::default(),
+            libp2p::tls::Config::new,
+            libp2p::yamux::Config::default,
+        )?
+        .with_behaviour(|_| ping::Behaviour::default())?
+        .with_swarm_config(|cfg| cfg.with_idle_connection_timeout(Duration::from_secs(u64::MAX))) // Allows us to observe pings indefinitely.
+        .build();
+
+    // Tell the swarm to listen on all interfaces and a random, OS-assigned
+    // port.
+    swarm.listen_on("/ip4/0.0.0.0/tcp/0".parse()?)?;
+
+    // Dial the peer identified by the multi-address given as the second
+    // command-line argument, if any.
+    if let Some(addr) = std::env::args().nth(1) {
+        let remote: Multiaddr = addr.parse()?;
+        swarm.dial(remote)?;
+        println!("Dialed {addr}")
+    }
+
+    loop {
+        match swarm.select_next_some().await {
+            SwarmEvent::NewListenAddr { address, .. } => println!("Listening on {address:?}"),
+            SwarmEvent::Behaviour(event) => println!("{event:?}"),
+            _ => {}
+        }
+    }
+}
